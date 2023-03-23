ebd3ba5c713ef455a025472294582fa527d280c9
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2023-03-23 01:14:50 +0000

    Fix bugs

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -13,8 +13,11 @@ migrate = Migrate(app, db)
 
 """ Domains ----------------------------------------------------------------- """
 
+# Company owned wallets
+
 class Wallet(db.Model):
     __tablename__ = "WALLETS"
+
     id   = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(80), unique=True, nullable=False)
 
@@ -22,29 +25,74 @@ class Wallet(db.Model):
     def mnemonic(self):
         return generate_mnemonic_from_seed(self.id)
 
+# Games that are buyable by player users
 
 class Game(db.Model):
     __tablename__ = "GAMES"
-    id   = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(80), unique=True, nullable=False)
-    cost = Column(Integer, nullable=False)
 
+    id       = Column(Integer, primary_key=True, autoincrement=True)
+    name     = Column(String(80), unique=True, nullable=False)
+    cost     = Column(Integer, nullable=False)
+    ipfsLink = Column(String(3000), nullable=False)
+
+# Player Users that buy games
+
+class User(db.Model):
+    __tablename__ = "USERS"
+
+    id       = Column(Integer, primary_key=True, autoincrement=True)
+    name     = Column(String(80), unique=True, nullable=False)
+
+class UserToGame(db.Model):
+    __tablename__ = "USER_TO_GAME"
+
+    userId = Column(Integer, ForeignKey("USERS.id"))
+    gameId = Column(Integer, ForeignKey("GAMES.id"))
+
+# Logs of all transactions
+
+class Invoice(db.Model): pass
 
 """ Routes ------------------------------------------------------------------ """
 
-@app.route("/user/new", methods=["POST"])
-def create_user():
+@app.route("/wallet/new", methods=["POST"])
+def create_wallet():
     
     req = request.get_json()
     name = req["name"]
 
     wallet = Wallet(name=name)
     db.session.add(wallet)
-
     db.session.commit()
 
     return {"id": wallet.id, "mnemonic": wallet.mnemonic}, 200
     
+@app.route("/user/new", methods=["POST"])
+def create_user():
+        
+    req = request.get_json()
+    name = req["name"]
+
+    user = User(name=name)
+    db.session.add(user)
+    db.session.commit()
+
+    return {"id": user.id}, 200
+
+@app.route("/user/buyGame", methods=["POST"])
+def buy_game(): # TODO - Add payment processing
+            
+    req = request.get_json()
+    userId = req["userId"]
+    gameId = req["gameId"]
+
+    # TODO - Receive payment info and connect to BTCPay
+
+    userToGame = UserToGame(userId=userId, gameId=gameId)
+    db.session.add(userToGame)
+    db.session.commit()
+
+    return {"userId": userId, "gameId": gameId}, 200
 
 
 if __name__ == "__main__":
